3d1175adc0d2f5364c5c6ed59e5cca02221c6c27
sam-yng <young142001@hotmail.com>
2023-11-19 12:59:12 +1100

    2352

--- 2352.ts
+++ 2352.ts
@@ -1,10 +1,20 @@
 function equalPairs(grid: number[][]): number {
-    let transposed: number[][] =  grid[0].map((col, c) => grid.map((row, r) => grid[r][c]))
+    // transpose the grid - from row x col, to col x
+    const transposed: number[][] =  grid[0].map((_, c) => grid.map((_, r) => grid[r][c]))
     let matches: number = 0
+
+    const areArraysEqual = <T>(arr1: T[], arr2: T[]): boolean => {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false
+            }
+        }
+        return true
+    }
 
     for (let i = 0; i < grid.length; i++) {
         for (let k = 0; k < transposed.length; k++) {
-            if (grid[i].toString() === transposed[k].toString()) {
+            if (areArraysEqual(grid[i], transposed[k])) {
                 matches += 1
             }
         }
